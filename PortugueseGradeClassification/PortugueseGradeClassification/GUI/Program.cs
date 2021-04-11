98076617291d929773f112deb017a17179f0d26a
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-04-10 20:39:26 -0500

    Forms MainWindow and LoadDataWindow added

--- PortugueseGradeClassification/PortugueseGradeClassification/GUI/Program.cs
+++ PortugueseGradeClassification/PortugueseGradeClassification/GUI/Program.cs
@@ -16,7 +16,7 @@ namespace PortugueseGradeClassification
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LoadDataWindow());
         }
     }
 }
---
+++ PortugueseGradeClassification/PortugueseGradeClassification/LoadDataWindow.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PortugueseGradeClassification
+{
+    public partial class LoadDataWindow : Form
+    {
+
+        MainWindow mainWindow;
+
+        public LoadDataWindow()
+        {
+            //falta controladora en clase mainwindow 
+            InitializeComponent();
+            mainWindow = new MainWindow();
+        }
+
+        private void LoadDataButton_Click(object sender, EventArgs e)
+        {
+            bool correctFile = true;
+            //cargar datos .csv 
+            //debe cambiar el boolean a false
+
+            if (correctFile)
+            {
+                this.Hide();
+                mainWindow.Show();
+            }
+        }
+    }
+}

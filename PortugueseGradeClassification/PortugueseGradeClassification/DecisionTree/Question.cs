13845bc5173f0355340b2f8e14131b03568dad92
Marco Vasquez <macroxx120@gmail.com>
2021-04-28 09:55:18 -0500

    created package DecisionTree and class Question

---
+++ PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/Question.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortugueseGradeClassification.DecisionTree
+{
+    class Question<T>
+    {
+        private int column;
+        private string[] row;
+        public object Value{get; private set; }
+
+
+        public Question(int column, object value)
+        {
+            this.column = column;
+            this.Value = value;
+        }
+
+        private bool IsNumeric(object obj)
+        {
+            return (obj is double || obj is int);//retorna si el objeto es numerico o no
+        }
+        /*
+        public bool compare(T )
+        {
+
+        }
+        */
+    }
+}

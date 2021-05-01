db33d76ec8eda00d3aa33201a752120baaef2d15
Marco Vasquez <macroxx120@gmail.com>
2021-05-01 18:39:31 -0500

    finished querty Class, this class could be changed though

--- PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/Question.cs
+++ PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/Question.cs
@@ -8,14 +8,14 @@ namespace PortugueseGradeClassification.DecisionTree
 {
     class Question<T>
     {
-        private int column;
+        public int Column{ get; private set; }
         private string[] row;
         public object Value{get; private set; }
 
 
         public Question(int column, object value)
         {
-            this.column = column;
+            this.Column = column;
             this.Value = value;
         }
 
@@ -23,11 +23,34 @@ namespace PortugueseGradeClassification.DecisionTree
         {
             return (obj is double || obj is int);//retorna si el objeto es numerico o no
         }
-        /*
-        public bool compare(T )
+        
+        public bool compare(T[] row)
         {
-
+            object rowValue = row[this.Column];
+            
+            if (IsNumeric(this.Value) && IsNumeric(rowValue))
+            {
+                return Convert.ToDouble(rowValue) >= Convert.ToDouble(Value);
+            }
+            else
+            {
+                return this.Value.Equals(rowValue);
+            }
         }
-        */
+
+        public override string ToString()
+        {
+            string message = "";
+            if (IsNumeric(Value))
+            {
+                message = $"Is col.{Column} >= {(double)Value}?";
+            }
+            else
+            {
+                message = $"Is col.{Column} == {(string)Value}?";
+            }
+            return message;
+        }
     }
 }
+

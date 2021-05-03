969a13c43952b97208418110a927b02bf6e5884b
Alejandro.F.Forero <54719974+AlejandroFonseca25@users.noreply.github.com>
2021-05-02 20:04:20 -0500

    Leaf class created

--- PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/Leaf.cs
+++ PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/Leaf.cs
@@ -8,5 +8,22 @@ namespace PortugueseGradeClassification.DecisionTree
 {
     class Leaf : NodeInterface
     {
+        public string[] rows { get; private set; } //Las filas que pertecen a la hoja
+        public int[] predictions { get; private set; } //Sujeto a cambiar dependiendo del arbol
+
+        public Leaf(string[] rows)
+        {
+            this.rows = rows;
+            //this.predictions = Metodo que obtiene los valores de interes de las filas
+        }
+
+        public string MakePrediction()
+        {
+            string message = "prediction: ";
+
+            //Llenado del mensaje, en proceso
+
+            return message;
+        }
     }
 }

5c437f65c0f13393777eb8530162a0d57bc2a1ea
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-05-05 12:10:36 -0500

    DecisionTree class created

---
+++ PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/DecisionTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortugueseGradeClassification.DecisionTree
+{
+    class DecisionTree<T>
+    {
+        private Node<T> root { get; set; }
+
+    }
+}

19b5d6af9028f5c28111b4023f761da694a29f05
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-05-10 11:25:44 -0500

    GraphView user control sucessfully implemented
    GUI divided into user controls

--- PortugueseGradeClassification/PortugueseGradeClassification/GUI/GraphView.Designer.cs
+++ PortugueseGradeClassification/PortugueseGradeClassification/GUI/GraphView.Designer.cs
@@ -54,16 +54,17 @@ namespace PortugueseGradeClassification.GUI
             "Internet access"});
             this.PortuComboBox.Location = new System.Drawing.Point(3, 188);
             this.PortuComboBox.Name = "PortuComboBox";
-            this.PortuComboBox.Size = new System.Drawing.Size(194, 22);
+            this.PortuComboBox.Size = new System.Drawing.Size(160, 22);
             this.PortuComboBox.TabIndex = 6;
             this.PortuComboBox.Text = "Field...";
+            this.PortuComboBox.SelectedIndexChanged += new System.EventHandler(this.PortuComboBox_SelectedIndexChanged);
             // 
             // label1
             // 
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(21, 169);
+            this.label1.Location = new System.Drawing.Point(4, 169);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(157, 16);
             this.label1.TabIndex = 5;
@@ -73,17 +74,16 @@ namespace PortugueseGradeClassification.GUI
             // 
             chartArea1.Name = "ChartArea1";
             this.PortuGraph.ChartAreas.Add(chartArea1);
-            this.PortuGraph.Dock = System.Windows.Forms.DockStyle.Fill;
             legend1.Name = "Legend1";
             this.PortuGraph.Legends.Add(legend1);
-            this.PortuGraph.Location = new System.Drawing.Point(0, 0);
+            this.PortuGraph.Location = new System.Drawing.Point(197, 20);
             this.PortuGraph.Name = "PortuGraph";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.PortuGraph.Series.Add(series1);
-            this.PortuGraph.Size = new System.Drawing.Size(976, 371);
+            this.PortuGraph.Size = new System.Drawing.Size(622, 324);
             this.PortuGraph.TabIndex = 4;
             this.PortuGraph.Text = "chart1";
             // 
@@ -99,7 +99,7 @@ namespace PortugueseGradeClassification.GUI
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(200, 371);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(166, 371);
             this.tableLayoutPanel1.TabIndex = 7;
             // 
             // GraphView

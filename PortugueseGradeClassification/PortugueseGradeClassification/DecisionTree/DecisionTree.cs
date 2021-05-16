aa3ba3a10860a3d91b7c86471d81c3c2fffb8b77
Diego-ds <54712482+Diego-ds@users.noreply.github.com>
2021-05-15 20:13:45 -0500

    BuildNode method in DecisionTree class done

--- PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/DecisionTree.cs
+++ PortugueseGradeClassification/PortugueseGradeClassification/DecisionTree/DecisionTree.cs
@@ -17,9 +17,16 @@ namespace PortugueseGradeClassification.DecisionTree
         {
         }
 
-        static public Node BuildNode()
+        static public Node BuildNode(String [] rows)
         {
-            return null;
+            Tuple<double, Question> bestQuestion = FindBestQuestion(rows);
+
+            if (bestQuestion.Item1 == 0) {
+                return new LeafNode(rows);
+            } 
+            Tuple<String[], String[]> truefalse_rows = Partitions(rows, bestQuestion.Item2);
+
+            return new DecisionNode(bestQuestion.Item2, truefalse_rows.Item1, truefalse_rows.Item2);
         }
 
         public LeafNode Classify(string row)
